f243cb0b3b31d12115a92cc9c00317a377b85f49
jamalazza <jamalazza@gmail.com>
2017-03-11 17:46:16 +0000

    first commit

---
+++ data/databases/activedirectory-database.js
@@ -0,0 +1,38 @@
+import ActiveDirectory from 'activedirectory';
+
+var config = {
+    url: 'ldap://192.168.1.3',
+    baseDN: 'OU=Organisation,DC=dimatit,DC=intra',
+    username: 'administrator@dimatit.intra',
+    password: 'Casanova2014',
+    includeMembership: [ 'user' ],
+    attributes: {
+        user: [
+            'displayName',
+            'mail',
+            'title',
+            'ipPhone',
+            'mobile',
+            'manager',
+            'thumbnailPhoto',
+            'employeeCin',
+            'codeMobile',
+            'sdaPhone',
+            'l',
+            'physicalDeliveryOfficeName'
+
+        ]
+    },
+    entryParser : customEntryParser
+};
+
+
+function customEntryParser(entry, raw, callback){
+    if (raw.hasOwnProperty("thumbnailPhoto")){
+        entry.thumbnailPhoto = raw.thumbnailPhoto;
+    }
+    callback(entry);
+}
+
+
+export const db = new ActiveDirectory(config);
